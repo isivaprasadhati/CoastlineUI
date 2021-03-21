2dd34ec6043d409281978fceeb36ef644175a0af
Sivaprasad Hati <sivaprasad10@outlook.com>
2021-03-21 14:42:12 +0530

    Sidenav implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Coastline';
+  mobileQuery: MediaQueryList;
+
+  fillerNav = Array.from({ length: 10 }, (_, i) => `Nav Item ${i + 1}`);
+
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener("change", () => {
+      this._mobileQueryListener
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeEventListener("change", () => {
+      this._mobileQueryListener
+    })
+  }
 }
